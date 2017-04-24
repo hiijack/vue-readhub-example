bf5bd704cab82ec99b97ba1fd71bf2fc868e1cbe
lunjz <jackerlun@gmail.com>
2017-04-24 23:42:40 +0800

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <Layout />
+  </div>
+</template>
+
+<script>
+import Layout from './components/Layout';
+
+export default {
+  name: 'app',
+  components: {
+    Layout
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+}
+body {
+  margin: 0;
+}
+</style>
