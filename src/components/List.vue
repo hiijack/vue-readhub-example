1cf78e38e498f4d9aea208a8616d87dd3f835609
lunjz <jackerlun@gmail.com>
2017-04-24 23:43:05 +0800

    Create List.vue

---
+++ src/components/List.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <div class="container" v-for="(item, index) in list" v-on:click="toggleInfo(item)">
+            <Item v-bind:item="item" v-bind:isLast="index != list.length - 1" />
+        </div>
+    </div>
+</template>
+
+<script>
+    import Item from './Item';
+
+    export default {
+        name: 'List',
+        props: ['list'],
+        components: {
+            Item
+        },
+        methods: {
+            toggleInfo(item) {
+                this.$emit('toggleInfo', item);
+            }
+        }
+    }
+
+</script>
+
+<style scoped>
+    .container {
+        margin-top: 15px;
+        margin-left: 15px;
+        margin-right: 15px;
+    }
+</style>
